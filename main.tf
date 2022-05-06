922326ddb8339ad2b5f20899735bb844a9cb7540
geborgeson <46825704+geborgeson@users.noreply.github.com>
2022-05-06 05:27:00 +0000

    adjust tf

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ data "azurerm_resource_group" "valheim" {
 
 resource "azurerm_kubernetes_cluster" "gb3cluster" {
   name                = "gb3scluster"
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.valheim.location
+  resource_group_name = data.azurerm_resource_group.valheim.name
   dns_prefix          = "exampleaks1"
 
   default_node_pool {
