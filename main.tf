4e4dc925809148591efb4095008169360fdde332
geborgeson <46825704+geborgeson@users.noreply.github.com>
2022-05-06 05:31:19 +0000

    adjust tf

--- main.tf
+++ main.tf
@@ -9,26 +9,5 @@ terraform {
 
 data "azurerm_resource_group" "valheim" {
   name     = "valheim"
-  location = "EastUS"
 }
 
-resource "azurerm_kubernetes_cluster" "gb3cluster" {
-  name                = "gb3scluster"
-  location            = data.azurerm_resource_group.valheim.location
-  resource_group_name = data.azurerm_resource_group.valheim.name
-  dns_prefix          = "exampleaks1"
-
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = {
-    Environment = "Production"
-  }
-}
