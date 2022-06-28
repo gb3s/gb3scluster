41b846881ac2a72c267f1f9d543d69eef7f58852
geborgeson <46825704+geborgeson@users.noreply.github.com>
2022-06-28 03:13:43 +0000

    adjust tf

--- aks/main.tf
+++ aks/main.tf
@@ -2,25 +2,23 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "~>3"
     }
   }
 }
 
-data "terraform_remote_state" "vpc" {
-  backend = "remote"
-  config = {
-    organization = "gb3s_infra"
-    workspaces = {
-      name = "gb3scluster"
-    }
-  }
+provider "azurerm" {
+  features {}
 }
 
 resource "azurerm_resource_group" "gb3s_group" {
   name     = "gb3s"
   location = "East US"
 }
+
+# resource "azurerm_managed_identity" "gb3s_cluster_id" {
+#   name  = "gb3s"
+# }
 
 # resource "azurerm_kubernetes_cluster" "gb3s_cluster" {
 #   name = "gb3s"
