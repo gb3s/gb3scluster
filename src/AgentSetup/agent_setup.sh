210139e5ac3c7334b567924ca61a3ea333844d0d
geborgeson <46825704+geborgeson@users.noreply.github.com>
2022-07-01 22:34:17 +0000

    add runner controller

--- src/AgentSetup/agent_setup.sh
+++ src/AgentSetup/agent_setup.sh
@@ -1,8 +1,13 @@
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
-helm install \
+helm upgrade \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --create-namespace \
   --version v1.8.2 \
   --set installCRDs=true
+  --install
+
+helm repo add actions-runner-controller https://actions-runner-controller.github.io/actions-runner-controller
+helm upgrade --install --namespace actions-runner-system --create-namespace \
+             --wait actions-runner-controller actions-runner-controller/actions-runner-controller
