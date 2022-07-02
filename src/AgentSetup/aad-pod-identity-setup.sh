7ac25ccffa9717c43bc0e5d816bfd5d9d9431fdb
geborgeson <46825704+geborgeson@users.noreply.github.com>
2022-07-02 12:29:24 +0000

    update for keyvault and aad setup

---
+++ src/AgentSetup/aad-pod-identity-setup.sh
@@ -0,0 +1,5 @@
+helm repo add aad-pod-identity https://raw.githubusercontent.com/Azure/aad-pod-identity/master/charts
+helm install aad-pod-identity aad-pod-identity/aad-pod-identity --set nmi.allowNetworkPluginKubenet=true 
+
+
+kubectl apply -f secretsProviderTest.yml
--- src/AgentSetup/agent_setup.sh
+++ src/AgentSetup/agent_setup.sh
@@ -10,4 +10,5 @@ helm upgrade \
 
 helm repo add actions-runner-controller https://actions-runner-controller.github.io/actions-runner-controller
 helm upgrade --install --namespace actions-runner-system --create-namespace \
-             --wait actions-runner-controller actions-runner-controller/actions-runner-controller
+             --wait actions-runner-controller actions-runner-controller/actions-runner-controller \
+             --set 
