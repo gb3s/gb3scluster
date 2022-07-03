0408b14aa43a52b3d0b3fbebb5a39b5f5fbc402c
geborgeson <46825704+geborgeson@users.noreply.github.com>
2022-07-03 15:04:29 +0000

    updaet setup script

--- src/AgentSetup/aad-pod-identity-setup.sh
+++ src/AgentSetup/aad-pod-identity-setup.sh
@@ -1,5 +1,3 @@
 helm repo add aad-pod-identity https://raw.githubusercontent.com/Azure/aad-pod-identity/master/charts
 helm install aad-pod-identity aad-pod-identity/aad-pod-identity --set nmi.allowNetworkPluginKubenet=true 
-
-
 kubectl apply -f secretsProviderTest.yml
