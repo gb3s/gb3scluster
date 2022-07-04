4fc02b0c5b36e01ad943674afbad09240887d4a7
geborgeson <46825704+geborgeson@users.noreply.github.com>
2022-07-04 12:15:53 +0000

    adjust tools script

--- src/AgentSetup/get_tools.sh
+++ src/AgentSetup/get_tools.sh
@@ -1,3 +1,7 @@
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+bash get_helm.sh
