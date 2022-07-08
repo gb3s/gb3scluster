4d1da5ce7edfba4548065d17a6959ebe27103544
geborgeson <46825704+geborgeson@users.noreply.github.com>
2022-07-08 22:41:28 +0000

    add helm apim deploy

--- src/AgentSetup/agent_setup.sh
+++ src/AgentSetup/agent_setup.sh
@@ -28,6 +28,6 @@ helm upgrade --install --namespace actions-runner-system  \
 kubectl apply -f orgRunner.yml
 
 helm install azure-api-management-gateway \
-             --set gateway.configuration.uri='gb3s-apim.configuration.azure-api.net' \
+             --set gateway.configuration.uri='gb3s-apim.configuration.azure-api.nethttps://gb3s-apim.management.azure-api.net/subscriptions/4dc1bd33-fef6-413d-aa31-bc639452a449/resourceGroups/gb3s/providers/Microsoft.ApiManagement/service/gb3s-apim?api-version=2021-01-01-preview' \
              --set gateway.auth.key='N0vb6CKyPXtDTfSf6AL5hTVL/RKfPx1FIvHAkDgsdlqbYuEMRhGtsc/25ELnywlDHJV5Tqh/aarmDJF4eAo8dg==' \
              azure-apim-gateway/azure-api-management-gateway
