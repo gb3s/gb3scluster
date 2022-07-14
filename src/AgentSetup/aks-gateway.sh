f66ceb7041d2114dd49bb52994556d36a1798fde
geborgeson <46825704+geborgeson@users.noreply.github.com>
2022-07-14 02:03:47 +0000

    adjust agic ingress

--- src/AgentSetup/aks-gateway.sh
+++ src/AgentSetup/aks-gateway.sh
@@ -2,7 +2,9 @@
 helm repo add application-gateway-kubernetes-ingress https://appgwingress.blob.core.windows.net/ingress-azure-helm-package/
 helm repo update
 
-helm install \
+helm upgrade \
+     --install \ 
+     --name ingress-azure \
      --namespace apim-ingress \
      --debug \
     #  --set appgw.name=applicationgatewayABCD \
