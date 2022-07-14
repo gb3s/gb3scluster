47973339c0deab28be74bc636a1d3974a5a8e89b
geborgeson <46825704+geborgeson@users.noreply.github.com>
2022-07-14 02:09:25 +0000

    adjust agic ingress

--- src/AgentSetup/aks-gateway.sh
+++ src/AgentSetup/aks-gateway.sh
@@ -1,3 +1,6 @@
+kubectl create serviceaccount --namespace kube-system tiller-sa
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller-sa
+helm init --tiller-namespace kube-system --service-account tiller-sa
 
 helm repo add application-gateway-kubernetes-ingress https://appgwingress.blob.core.windows.net/ingress-azure-helm-package/
 helm repo update
