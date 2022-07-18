0c46169f2e2c9b785646337a3c0a91b463bbacc9
geborgeson <46825704+geborgeson@users.noreply.github.com>
2022-07-18 16:05:45 +0000

    adjust runner agent setup

--- src/AgentSetup/action_runner_agent_setup.sh
+++ src/AgentSetup/action_runner_agent_setup.sh
@@ -1,4 +1,4 @@
 cluster_name=gb3s
 sudo cp action_runner_agent.yml "$cluster_name"_action_runner_agent.yml
 sudo sed -i "s#CLUSTER_NAME#$cluster_name#g" "$cluster_name"_action_runner_agent.yml
-kubectl apply "$cluster_name"_action_runner_agent.yml -n actions-runner-system
+kubectl apply -f "$cluster_name"_action_runner_agent.yml -n actions-runner-system 
