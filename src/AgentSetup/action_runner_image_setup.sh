5fdf11df9a09fba69ab7f741a7797b2ca6736dc5
geborgeson <46825704+geborgeson@users.noreply.github.com>
2022-07-18 15:42:56 +0000

    fix build line

--- src/AgentSetup/action_runner_image_setup.sh
+++ src/AgentSetup/action_runner_image_setup.sh
@@ -1,4 +1,4 @@
 sudo az acr login -n gb3sacr
-sudo docker build -t "$cluster_name"runner:latest .
+sudo docker build -t "$cluster_name"/runner:latest .
 sudo docker tag "$cluster_name"/runner:latest ""$cluster_name"acr.azurecr.io/"$cluseter_name"/runner:latest"
 sudo docker push "$cluster_name"/runner:latest
