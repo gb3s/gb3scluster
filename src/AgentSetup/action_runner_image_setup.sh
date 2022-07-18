b4b20cc6d3cb55db4d53db27897e91d0e1f77a1f
geborgeson <46825704+geborgeson@users.noreply.github.com>
2022-07-18 15:43:39 +0000

    add cluster name var

--- src/AgentSetup/action_runner_image_setup.sh
+++ src/AgentSetup/action_runner_image_setup.sh
@@ -1,3 +1,5 @@
+cluster_name=gb3s
+
 sudo az acr login -n gb3sacr
 sudo docker build -t "$cluster_name"/runner:latest .
 sudo docker tag "$cluster_name"/runner:latest ""$cluster_name"acr.azurecr.io/"$cluseter_name"/runner:latest"
