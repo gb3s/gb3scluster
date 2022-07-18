fbfe576ad21b8cac04558ff46f149e3d98b11e38
geborgeson <46825704+geborgeson@users.noreply.github.com>
2022-07-18 15:36:48 +0000

    add acr login to docker setup

--- src/AgentSetup/agent_setup.sh
+++ src/AgentSetup/agent_setup.sh
@@ -21,6 +21,7 @@ helm upgrade \
   --install
 
 ###Actions Runner Image Setup
+sudo az acr login -n gb3sacr
 sudo docker build -t "$cluster_name"runner:latest .
 sudo docker tag "$cluster_name"\runner:latest ""$cluster_name"acr.azurecr.io/"$cluseter_name"/runner:latest"
 sudo docker push "$cluster_name"/runner:latest
